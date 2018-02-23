3983b4dbda0c1521ec76b66eaf266dffadc2ff1a
Richard Lee <rlee@tokyo3.com>
2018-02-22 22:57:18 -0800

    removed a number of unnecessary intermediate methods

--- Bigint/Sources/Bigint/Bigint.swift
+++ Bigint/Sources/Bigint/Bigint.swift
@@ -8,19 +8,19 @@ func == (left: Bigint, right: Bigint) -> Bool {
 }
 
 func == (left: Bigint, right: Int) -> Bool {
-    return left.equals(right)
+    return left.equals(Bigint(right))
 }
 
 func == (left: Int, right: Bigint) -> Bool {
-    return right.equals(left)
+    return right.equals(Bigint(left))
 }
 
 func == (left: Bigint, right: UInt) -> Bool {
-    return left.equals(right)
+    return left.equals(Bigint(right))
 }
 
 func == (left: UInt, right: Bigint) -> Bool {
-    return right.equals(left)
+    return right.equals(Bigint(left))
 }
 
 struct Bigint {
@@ -28,25 +28,13 @@ struct Bigint {
     var content1: UInt64 = 0
     var negative: Bool = false
 
-    init(_ other: Bigint) {
-        self.set(other)
-    }
-
-    init(_ other: UInt) {
-        self.set(other)
-    }
-
-    init(_ other: Int) {
-        self.set(other)
-    }
-
-    mutating func set(_ value: Bigint) {
+    init(_ value: Bigint) {
         self.content0 = value.content0
         self.content1 = value.content1
         self.negative = value.negative
     }
 
-    mutating func set(_ value: Int) {
+    init(_ value: Int) {
         var absolute = value
         self.negative = false
         if absolute < 0 {
@@ -57,36 +45,17 @@ struct Bigint {
         self.content1 = 0
     }
 
-    mutating func set(_ value: UInt) {
+    init(_ value: UInt) {
         self.negative = false
         self.content0 = UInt64(value)
         self.content1 = 0
     }
-    
+
     func equals(_ other: Bigint) -> Bool {
         return self.content0 == other.content0 &&
                self.content1 == other.content1 &&
                self.negative == other.negative
     }
 
-    func equals(_ other: Int) -> Bool {
-        var absolute = other
-        var negative = false
-        if other < 0 {
-            negative = true
-            absolute = -1 * other
-        }
-
-        return self.content0 == absolute &&
-               self.content1 == 0 &&
-               self.negative == negative
-    }
-
-    func equals(_ other: UInt) -> Bool {
-
-        return self.content0 == other &&
-               self.content1 == 0 &&
-               self.negative == false
-    }
 
 }
